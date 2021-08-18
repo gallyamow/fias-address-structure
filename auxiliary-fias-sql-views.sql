72da144f71adf56be0db766a5653be32400a60ef
Ramil Gallyamov <gallyamov.ramil@tatar.ru>
2021-08-18 15:41:49 +0300

    cleaner auxiliary views

--- auxiliary-fias-sql-views.sql
+++ auxiliary-fias-sql-views.sql
@@ -4,7 +4,7 @@
 --  здесь таблица gar.adm_hierarchy используется как связующее звено всех видов объектов из всех таблиц
 --  (addr_obj, apartments, houses, carplaces, rooms, steads)
 
--- tree
+-- tree [
 DROP MATERIALIZED VIEW IF EXISTS gar.v_adm_hierarchy_path;
 CREATE MATERIALIZED VIEW gar.v_adm_hierarchy_path AS
 WITH RECURSIVE nodes(hierarchy_id, object_id, path, is_active) AS (
@@ -31,35 +31,22 @@ FROM nodes;
 -- См. object_id=6092
 CREATE INDEX ON gar.v_adm_hierarchy_path(hierarchy_id);
 CREATE INDEX ON gar.v_adm_hierarchy_path(object_id);
-
 CREATE INDEX ON gar.v_adm_hierarchy_path USING GIST(path_ltree);
 -- не потребовался?
+-- ]
 
--- связанные с каждым элементом иерархии (через объект) данные (hierarchy_id)
-DROP MATERIALIZED VIEW IF EXISTS gar.v_adm_hierarchy_relation;
-CREATE MATERIALIZED VIEW gar.v_adm_hierarchy_relation AS
-SELECT t.id AS hierarchy_id,
-    t.objectid AS object_id,
-    t.relation_id,
-    t.relation_is_active,
-    t.relation_is_actual,
-    CASE WHEN t.addr_obj IS NOT NULL
-             THEN 'addr_obj'
-         WHEN t.house IS NOT NULL
-             THEN 'house'
-         WHEN t.apartment IS NOT NULL
-             THEN 'apartment'
-         WHEN t.room IS NOT NULL
-             THEN 'room'
-         WHEN t.carplace IS NOT NULL
-             THEN 'carplace'
-         WHEN t.stead IS NOT NULL
-             THEN 'stead'
-    END AS relation_type,
-    COALESCE(t.addr_obj, t.house, t.room, t.apartment, t.carplace, t.stead) AS relation_data
+-- Список всех объектов. Каждый объект представлен 1 раз, содержит все свои relations
+-- v_objects [
+DROP MATERIALIZED VIEW IF EXISTS gar.v_objects;
+CREATE MATERIALIZED VIEW gar.v_objects AS
+SELECT t.objectid AS object_id,
+    array_agg(DISTINCT t.type) AS types,
+    array_to_json(array_agg(jsonb_build_object('id', t.relation_id, 'is_active', t.relation_is_active, 'is_actual',
+                                               t.relation_is_actual, 'type', t.type, 'data',
+                                               COALESCE(t.addr_obj, t.house, t.room, t.apartment, t.carplace,
+                                                        t.stead)))) AS relations
 FROM (
-         SELECT hr.id,
-             hr.objectid,
+         SELECT ho.objectid,
              row_to_json(addr_obj) AS addr_obj,
              row_to_json(houses) AS house,
              row_to_json(apartments) AS apartment,
@@ -70,49 +57,45 @@ FROM (
              COALESCE(addr_obj.isactive, houses.isactive, rooms.isactive, apartments.isactive, carplaces.isactive,
                       steads.isactive) AS relation_is_active,
              COALESCE(addr_obj.isactual, houses.isactual, rooms.isactual, apartments.isactual, carplaces.isactual,
-                      steads.isactual) AS relation_is_actual
-         FROM gar.adm_hierarchy hr
-                  LEFT JOIN gar.addr_obj ON addr_obj.objectid = hr.objectid
-                  LEFT JOIN gar.houses ON houses.objectid = hr.objectid
-                  LEFT JOIN gar.rooms ON rooms.objectid = hr.objectid
-                  LEFT JOIN gar.apartments ON apartments.objectid = hr.objectid
-                  LEFT JOIN gar.carplaces ON carplaces.objectid = hr.objectid
-                  LEFT JOIN gar.steads ON steads.objectid = hr.objectid
-     ) AS t;
+                      steads.isactual) AS relation_is_actual,
+             CASE WHEN addr_obj.id IS NOT NULL
+                      THEN 'addr_obj'
+                  WHEN houses.id IS NOT NULL
+                      THEN 'house'
+                  WHEN apartments.id IS NOT NULL
+                      THEN 'apartment'
+                  WHEN rooms.id IS NOT NULL
+                      THEN 'room'
+                  WHEN carplaces.id IS NOT NULL
+                      THEN 'carplace'
+                  WHEN steads.id IS NOT NULL
+                      THEN 'stead'
+             END AS type
+         FROM (
+                  SELECT DISTINCT hr.objectid
+                  FROM gar.adm_hierarchy hr
+              ) as ho
+                  LEFT JOIN gar.addr_obj ON addr_obj.objectid = ho.objectid
+                  LEFT JOIN gar.houses ON houses.objectid = ho.objectid
+                  LEFT JOIN gar.rooms ON rooms.objectid = ho.objectid
+                  LEFT JOIN gar.apartments ON apartments.objectid = ho.objectid
+                  LEFT JOIN gar.carplaces ON carplaces.objectid = ho.objectid
+                  LEFT JOIN gar.steads ON steads.objectid = ho.objectid
+     ) AS t
+GROUP BY t.objectid;
+CREATE UNIQUE INDEX ON gar.v_objects(object_id);
+-- GIN index for types or expression index ?
+-- ] v_objects
 
-CREATE INDEX ON gar.v_adm_hierarchy_relation(hierarchy_id); -- неуникальный, потому что могут быть переименования домов и тд.  (@see objectid=70743973)
-CREATE INDEX ON gar.v_adm_hierarchy_relation(object_id);
-
--- связанные с каждым элементом иерархии параметры (hierarchy_id)
-DROP MATERIALIZED VIEW IF EXISTS gar.v_adm_hierarchy_actual_params;
-CREATE MATERIALIZED VIEW gar.v_adm_hierarchy_actual_params AS
-SELECT hr.id AS hierarchy_id,
-    hr.objectid AS object_id,
+-- Связанные с каждым объектом параметры
+DROP MATERIALIZED VIEW IF EXISTS gar.v_object_params;
+CREATE MATERIALIZED VIEW gar.v_object_params AS
+SELECT p.objectid AS object_id,
     array_to_json(array_agg(
-            jsonb_build_object('type_id', p.typeid, 'value', p.value, 'start_date', p.startdate, 'end_date',
-                               p.enddate))) AS values
-FROM gar.adm_hierarchy hr
-         JOIN gar.param AS p ON p.objectid = hr.objectid AND p.changeidend = 0
-GROUP BY hr.id;
-CREATE INDEX ON gar.v_adm_hierarchy_actual_params(object_id);
--- TODO: индексы по object_id вроде больше не нужны
+            jsonb_build_object('type_id', p.typeid, 'value', p.value, 'start_date', p.startdate, 'end_date', p.enddate,
+                               'is_actual', p.changeidend = 0))) AS values
+FROM gar.param p
+GROUP BY p.objectid;
+CREATE UNIQUE INDEX ON gar.v_object_params(object_id);
+-- ] v_object_params
 
--- denormalized gar.v_adm_hierarchy_path
-DROP MATERIALIZED VIEW IF EXISTS gar.v_adm_hierarchy_path_ext;
-CREATE MATERIALIZED VIEW gar.v_adm_hierarchy_path_ext AS
-SELECT p.*,
-    (
-        SELECT array_to_json(array_agg(jsonb_build_object('relation', row_to_json(r2.*), 'params', (
-                                                                                                       SELECT array_to_json(array_agg(row_to_json(params.*)))
-                                                                                                       FROM gar.v_adm_hierarchy_actual_params AS params
-                                                                                                       WHERE params.object_id = r2.object_id
-                                                                                                   ))))
-        FROM unnest(p.path) AS pid
-                 JOIN gar.adm_hierarchy AS hr2
-        on hr2.objectid = pid AND hr2.isactive = 1 -- для каждого объекта из пути мы находим актуальное положение в иерархии
-            -- (переходы по иерархии мы таким образом игнорируем)
-                 JOIN gar.v_adm_hierarchy_relation r2 ON r2.hierarchy_id = hr2.id -- затем получаем все relations к этому положению
-        -- (переименования в том числе)
-    ) AS parents
-FROM gar.v_adm_hierarchy_path AS p;
-CREATE INDEX ON gar.v_adm_hierarchy_path_ext(hierarchy_id);
